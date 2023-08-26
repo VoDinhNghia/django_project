2882b6fba838e86b1afc418244f952feed86a6b8
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-26 20:06:57 +0700

    create model and form action

--- django_app/models.py
+++ django_app/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True, unique=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField()
+
+    def get_absolute_url(self):
+        return reverse('blog_post_detail', args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+    
+    class Meta:
+        ordering = ['created_at']
+        def __unicode__(self):
+            return self.title
--- django_app/views.py
+++ django_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django_app.models import Post
 
 def homePage(request):
     return render(request, "home.html", {"data": "test data"})
+
+def createPost(request):
+    return render(request, "createNewPost.html")
+
+def createNewPost(request):
+    print("test call api")
+    return render(request, "home.html", {"data": "hello"})
