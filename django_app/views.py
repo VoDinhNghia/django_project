8dbce9fbcdf6ef9439eed232ab7432a138c29925
VoDinhNghia <vodinhnghia85@gmail.com>
2025-07-17 19:54:21 +0700

    create intro page and update file name

--- django_app/views.py
+++ django_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django_app.models import Post
+from django.contrib.auth.forms import AuthenticationForm
 
 def homePage(request):
     return render(request, "home.html", {"data": "test data"})
 
 def createPost(request):
-    return render(request, "createNewPost.html")
+    return render(request, "create_new_post.html")
 
 def createNewPost(request):
     print("test call api")
     return render(request, "home.html", {"data": "hello"})
+
+def login_view(request):
+    form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
