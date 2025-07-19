1b01eb0c3ff3e9a219a63e542bdab114f543ed0e
VoDinhNghia <vodinhnghia85@gmail.com>
2025-07-19 20:21:18 +0700

    create file .env and upload image page

--- django_app/urls.py
+++ django_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name='test_view'),
+    path('', views.homePage, name='home-view'),
     path('create-post', views.createPost, name='create-post'),
-    path('api-create-post', views.createNewPost, name='api-create-post')
+    path('api-create-post', views.createNewPost, name='api-create-post'),
+    path('simple-upload', views.simpleUpload, name='simple-upload')
 ]
--- django_app/views.py
+++ django_app/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from django_app.models import Post
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.files.storage import FileSystemStorage
+from dotenv import load_dotenv
+import os
 
 def homePage(request):
-    return render(request, "home.html", {"data": "test data"})
+    return render(request, "home.html", {"data": "test dd data"})
 
 def createPost(request):
     return render(request, "create_new_post.html")
 
 def createNewPost(request):
-    print("test call api")
     return render(request, "home.html", {"data": "hello"})
 
-def login_view(request):
+def loginView(request):
     form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
+
+def simpleUpload(request):
+    load_dotenv()
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'simple_upload.html', {
+            'data': os.getenv("URL_HOST_LOCAL")+uploaded_file_url
+        })
+    return render(request, 'simple_upload.html')
+
+def handleImage():
+    print('hande image upload')
